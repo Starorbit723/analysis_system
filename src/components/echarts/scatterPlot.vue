a113304c0297126d7c9e984895a0c6c6bb081fe2
yangyadong.fang <yangyadong.fang@bitmain.com>
2018-11-21 14:42:23 +0800

    散点图

---
+++ src/components/echarts/scatterPlot.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="scatterPlot_content" ref="scatterPlot"></div>
+</template>
+
+<script>
+export default {
+  props:['scatterPlotData'],
+  data () {
+    return {
+      echarts:''
+    }
+  },
+  mounted () {
+    this.$nextTick(this.echartsInit)
+  },
+  methods: {
+    echartsInit () {
+      let self = this
+      let initEcharts = self.$refs.scatterPlot
+      self.echarts = self.$echarts.init(initEcharts)
+      self.echarts.setOption({
+				title: {
+          text: self.scatterPlotData.title,
+          left:'center',
+          textStyle:{
+            fontSize: '20',
+            color: '#266099'
+          }
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          type: 'scatter',
+          data: [120, 200, 150, 80, 70, 110, 130]
+        }]
+      })
+    }
+  }
+}
+</script>
+
+<style>
+  .scatterPlot_content{
+    width:800px;
+    height: 500px;
+  }
+</style>
--- src/pages/analysisEcharts.vue
+++ src/pages/analysisEcharts.vue
@@ -3,6 +3,7 @@
     <Header></Header>
     <section class="main_body">
       <div class="content_warpper">
+        <ScatterPlot :scatterPlotData="scatterPlotData"></ScatterPlot>
       </div>
     </section>
     <Footer :footerFixed="footerFixed"></Footer>
@@ -12,16 +13,23 @@
 <script>
 import Header from '@components/header'
 import Footer from '@components/footer'
+import ScatterPlot from '@components/echarts/scatterPlot'
 
 export default {
-  components: {Header, Footer},
+  components: {Header, Footer, ScatterPlot},
   data () {
     return {
-      footerFixed: true
+      footerFixed: true,
+      scatterPlotData: {
+        title: 'this is a title',
+        data:[]
+      }
     }
   },
   mounted () {
     console.log(this.$route.params.analysisForm)
+  },
+  methods: {
   }
 }
 </script>
